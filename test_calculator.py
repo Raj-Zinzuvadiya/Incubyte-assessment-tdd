154f62a3471be711df43385693f0bd443f2be94a
Raj-Zinzuvadiya <zinzuvadiya.r2000@gmail.com>
2024-03-28 16:26:58 +0530

    Added first failing test case

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestCalculator(unittest.TestCase):
+    
+    def test_nothing(self):
+        self.fail()
+        
+
+if __name__ == "__main__":
+    unittest.main()
+    
