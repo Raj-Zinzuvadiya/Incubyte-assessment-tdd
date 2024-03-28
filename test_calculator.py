07a31a56951fe650d22c1cc89893f9955f41a2ce
Raj-Zinzuvadiya <zinzuvadiya.r2000@gmail.com>
2024-03-28 16:33:55 +0530

    Added testcase for one number

--- test_calculator.py
+++ test_calculator.py
@@ -4,6 +4,7 @@ class TestCalculator(unittest.TestCase):
     
     def test_add(self):
         self.assertEqual(self.add(""), 0)
+        self.assertEqual(self.add("8"), 8)
         
     def add(self, numbers: str) -> int:
         return 0
