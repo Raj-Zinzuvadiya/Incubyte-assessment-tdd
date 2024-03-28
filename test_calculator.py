30d98b7bbd229bba23599e3d89155577af2aca5f
Raj-Zinzuvadiya <zinzuvadiya.r2000@gmail.com>
2024-03-28 16:31:30 +0530

    Added dd method

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,9 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.add(""), 0)
         
+    def add(self, numbers: str) -> int:
+        return 0
+        
 
 if __name__ == "__main__":
     unittest.main()
